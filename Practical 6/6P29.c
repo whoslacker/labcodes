8f11b18aec8cc2552bfb7fa215f1765cdac72f75
whoslacker <heykcs@gmail.com>
2025-04-12 18:46:34 +0530

    Delete Practical 6 directory

--- Practical 6/6P29.c
+++
@@ -1,9 +0,0 @@
-// Right-Angled Triangle
-#include <stdio.h>
-int main() {
-    for (int i = 1; i <= 5; i++) {
-        for (int j = 1; j <= i; j++) printf("%d ", j);
-        printf("\n");
-    }
-    return 0;
-}
--- Practical 6/6P30.c
+++
@@ -1,9 +0,0 @@
-// Inverted Right-Angled Triangle
-#include <stdio.h>
-int main() {
-    for (int i = 5; i >= 1; i--) {
-        for (int j = 1; j <= i; j++) printf("* ");
-        printf("\n");
-    }
-    return 0;
-}
